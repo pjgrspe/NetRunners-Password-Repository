fbadb1735e0bb9190d328104a74feeee717cec9c
Jason Gorospe <9.pjgorospe@gmail.com>
2023-05-09 03:50:34 +0800

    Dashboard index layout updates

--- PasswordRepository/Controllers/DashboardController.cs
+++ PasswordRepository/Controllers/DashboardController.cs
@@ -9,10 +9,14 @@ namespace PasswordRepository.Controllers
     public class DashboardController : Controller
     {
         // GET: Dashboard
-        //public ActionResult Index() // FOR MAIN PROD
-        public ActionResult Test() // FOR DEBUGGING
-        {
-            return View();
-        }
+        public ActionResult Index() // FOR MAIN PROD
+            {
+                return View();
+            }
+
+        //public ActionResult Test() // FOR DEBUGGING
+        //{
+          //  return View();
+        //}
     }
 }
