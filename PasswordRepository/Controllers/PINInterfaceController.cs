23c287401063808cdc8a7e0f41bdd9bea55ccbea
Jason Gorospe <9.pjgorospe@gmail.com>
2023-05-09 18:51:37 +0800

    PIN Interface test

---
+++ PasswordRepository/Controllers/PINInterfaceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PasswordRepository.Controllers
+{
+    public class PINInterfaceController : Controller
+    {
+        // GET: PINInterface
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
