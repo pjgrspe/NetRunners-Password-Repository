92196a135f71902caf297348a0ee1758b0f4cd0e
Jason Gorospe <9.pjgorospe@gmail.com>
2023-06-08 15:56:33 +0800

    Admin interface dashboard

---
+++ PasswordRepository/Controllers/AdminInterfaceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PasswordRepository.Controllers
+{
+    public class AdminInterfaceController : Controller
+    {
+        // GET: AdminInterface
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
