492fce79d5b15674325c7e77d3c3e4f647cab3e6
Jason Gorospe <9.pjgorospe@gmail.com>
2023-06-13 13:28:13 +0800

    Session updates

--- PasswordRepository/Controllers/SettingsController.cs
+++ PasswordRepository/Controllers/SettingsController.cs
@@ -132,6 +132,11 @@ namespace PasswordRepository.Controllers
                 userDetails.LASTNAME = userLN;
                 userDetails.DATE_MODIFIED = DateTime.Now;
 
+                //Sets the Session variables to the corresponding data on the table
+                Session["UserName"] = userLogin.USERNAME;
+                Session["FullName"] = userDetails.FIRSTNAME + " " + userDetails.LASTNAME;
+                Session["Email"] = userLogin.EMAIL;
+
                 //Saves the changes
                 if (entities.SaveChanges() >= 1)
                 {
@@ -168,6 +173,10 @@ namespace PasswordRepository.Controllers
                 //Sets selected entry's data to the information passed through the function
                 userDetails.PIN = EncryptedPIN;
                 userDetails.TIMEOUT = userTO;
+
+
+                //Sets the Session variables PIN and TO to the corresponding data on the table
+                Session["PIN"] = userDetails.PIN;
                 Session["TO"] = userDetails.TIMEOUT;
 
                 //Saves the changes
